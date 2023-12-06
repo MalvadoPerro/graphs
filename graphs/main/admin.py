35327628e1585313b970542223c0f8425565221c
MalvadoPerro <114355656+MalvadoPerro@users.noreply.github.com>
2023-12-06 15:59:35 +0300

    Add files via upload

--- graphs/main/admin.py
+++ graphs/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Count
+from .models import Edges
+from .models import Names
 
-# Register your models here.
+admin.site.register(Count)
+admin.site.register(Edges)
+admin.site.register(Names)
