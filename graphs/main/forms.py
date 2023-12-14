9e1b83c167109a70006d5ddb595bb5ba746a5051
MalvadoPerro <114355656+MalvadoPerro@users.noreply.github.com>
2023-12-14 15:15:22 +0300

    Add files via upload

--- graphs/main/forms.py
+++ graphs/main/forms.py
@@ -1,5 +1,7 @@
 from .models import Count
-from django.forms import ModelForm, NumberInput
+from .models import Edges
+from .models import Names
+from django.forms import ModelForm, NumberInput, TextInput,CheckboxInput, RadioSelect
 
 class CountForm(ModelForm):
     class Meta:
@@ -15,4 +17,37 @@ class CountForm(ModelForm):
                 'step': '1',
                 'list': 'tickmarks'
             })
+        }
+
+class EdgesForm(ModelForm):
+    class Meta:
+        model = Edges
+        fields = ['out', 'to', 'weight']
+
+        widgets = {
+
+        }
+
+class NamesForm(ModelForm):
+    class Meta:
+        model = Names
+        fields = ['num', 'name', 'dangerous']
+
+        widgets = {
+            'num': NumberInput(attrs={
+
+            }),
+
+            'name': TextInput(attrs={
+                'type': 'text',
+                'placeholder': 'Новое название',
+                'class': 'name_change'
+            }),
+
+            'dangerous': RadioSelect(
+                attrs={
+                'type': 'radio',
+                'name': 'dangerous'
+            },
+                choices={'True': 'Да', 'False': 'Нет'}, )
         }
--- graphs/main/views.py
+++ graphs/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.template.defaulttags import register
 from .models import Count
-from .forms import CountForm
+from .forms import CountForm, NamesForm
 
 @register.filter
 def get_range(value):
@@ -23,7 +23,19 @@ def index(request):
 
 def param(request):
     cont = Count.objects.all()[:1]
-    return render(request, 'main/params.html', {'cont': cont})
+
+    if request.method == 'POST':
+        form_name = NamesForm(request.POST)
+        form_name.save()
+
+    form_name = NamesForm()
+
+    data = {
+        'cont': cont,
+        'form_name': form_name
+    }
+
+    return render(request, 'main/params.html', data)
 
 def visual(request):
     return render(request, 'main/visual.html')
