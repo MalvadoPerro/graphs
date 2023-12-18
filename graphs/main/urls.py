a4396bc179f225a45590d3e91397a036133c60a6
MalvadoPerro <114355656+MalvadoPerro@users.noreply.github.com>
2023-12-19 02:37:49 +0300

    Add files via upload

--- graphs/main/urls.py
+++ graphs/main/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='main'),
     path('params', views.params, name='params'),
     path('visual', views.visual, name='visual'),
-    path('param/<int:pk>', views.Param.as_view(), name='param')
+    path('<int:pk>', views.Param.as_view(), name='param')
 ]
